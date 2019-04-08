143dca1bed0202292fa006153d921502d1a9368e
Tyler Sheaffer <ty.sheaffer@gmail.com>
2019-04-08 16:51:06 -0700

    get gobuild.sh working

--- gobuild.sh
+++ gobuild.sh
@@ -3,12 +3,12 @@
 set -eux
 
 export GOPATH="$(pwd)/.gobuild"
-SRCDIR="${GOPATH}/src/github.com/mickep76/tf"
+SRCDIR="${GOPATH}/src/github.com/calm/vbump"
 
 [ -d ${GOPATH} ] && rm -rf ${GOPATH}
 mkdir -p ${GOPATH}/{src,pkg,bin}
 mkdir -p ${SRCDIR}
-cp tf.go ${SRCDIR}
+cp vbump.go ${SRCDIR}
 (
     echo ${GOPATH}
     cd ${SRCDIR}
